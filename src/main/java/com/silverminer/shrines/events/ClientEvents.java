2d3006214583e0eaba284e8d4f975ef87e3cb705
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2021-05-18 15:51:55 +0200

    Abstracted bound drawing into extra method (draw does still not work)

--- src/main/java/com/silverminer/shrines/events/ClientEvents.java
+++ src/main/java/com/silverminer/shrines/events/ClientEvents.java
@@ -25,11 +25,9 @@ import com.silverminer.shrines.structures.custom.helper.ResourceData;
 import com.silverminer.shrines.utils.custom_structures.Utils;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.math.vector.Vector3d;
@@ -48,76 +46,76 @@ public class ClientEvents {
 	public static class ForgeEventBus {
 		@SubscribeEvent
 		/**
-		 * TODO Bounds moves with animations (relative to the blocks) flying causes
-		 * errors -> standing not -> Seems not to happen at blovkhighlight so may
-		 * activeRenderInfo?
+		 * TODO Something went wrong with matrixstack. everything else is similar to
+		 * onBlockHighlight which works perfectly
+		 * 
+		 * I wish to call my render function here to draw it everytime Here is the frame
+		 * drawn wrong: The color is much darker than wished (Dark Green/Black instead
+		 * of White) and the bound moves while flying
 		 * 
 		 * @param event
 		 */
 		public static void renderWorldLast(RenderWorldLastEvent event) {
+			renderBounds(event.getMatrixStack());
+		}
+
+		@SubscribeEvent
+		/**
+		 * This is only a test function. Here is the frame correctly drawn. Color
+		 * matches (White - 0xffffff) and the bounds aren't moving while flying
+		 * 
+		 * @param event
+		 */
+		public static void renderWorldLast(DrawHighlightEvent.HighlightBlock event) {
+			// renderBounds(event.getMatrix());
+		}
+
+		public static void renderBounds(MatrixStack ms) {
+			// Enable depth test first
 			RenderSystem.depthMask(false);
+
+			// Get Minecraft and store it locally for multiple use
 			Minecraft mc = Minecraft.getInstance();
+
+			// Get dimension to draw only bounds of the correct dimension
 			RegistryKey<World> dim;
 			if (mc.level != null) {
 				dim = mc.level.dimension();
 			} else {
 				dim = null;
 			}
-			MatrixStack ms = event.getMatrixStack();
-			ActiveRenderInfo activerenderinfo = mc.gameRenderer.getMainCamera();
-			activerenderinfo.setup(mc.level, (Entity) (mc.getCameraEntity() == null ? mc.player : mc.getCameraEntity()),
-					!mc.options.getCameraType().isFirstPerson(), mc.options.getCameraType().isMirrored(),
-					event.getPartialTicks());
-			Vector3d vec = activerenderinfo.getPosition();
+
+			// This doesn't work in both ways. What's the correct / working way to do that?
+			RenderSystem.lineWidth(30.0f);
+
+			// Translate coordinates from players system to world system
+			Vector3d vec = mc.gameRenderer.getMainCamera().getPosition();
 			double renderPosX = vec.x();
 			double renderPosY = vec.y();
 			double renderPosZ = vec.z();
-			IRenderTypeBuffer irendertypebuffer1 = mc.renderBuffers().bufferSource();
-			IVertexBuilder vb = irendertypebuffer1.getBuffer(RenderType.lines());
 			ms.pushPose();
 			ms.translate(-renderPosX, -renderPosY, -renderPosZ);
+
+			// Get vertex builder
+			IRenderTypeBuffer irendertypebuffer1 = mc.renderBuffers().bufferSource();
+			IVertexBuilder vb = irendertypebuffer1.getBuffer(RenderType.lines());
+
+			// Color of the bound (White)
 			Color c = new Color(Utils.properties.bound_color);
+			// Split up in red, green and blue and transform it to 0.0 - 1.0
 			float red = c.getRed() / 255.0f;
 			float green = c.getGreen() / 255.0f;
 			float blue = c.getBlue() / 255.0f;
+
+			// Iterate over all bounds to draw
 			for (CustomStructureData data : Utils.DATAS_FROM_SERVER) {
 				for (ResourceData rd : data.PIECES_ON_FLY) {
 					if (rd.getDimension() == dim) {
 						MutableBoundingBox mbb = rd.getBounds();
+						// Drawing a line box as in StructureTileEntityRenderer#render
 						WorldRenderer.renderLineBox(ms, vb, mbb.x0, mbb.y0, mbb.z0, mbb.x1, mbb.y1, mbb.z1, red, green,
-								blue, 1.0f);
+								blue, 1.0f, red, green, blue);
 					}
-				}
-			}
-			ms.popPose();
-		}
-
-		// @SubscribeEvent
-		/**
-		 * 
-		 * @param event
-		 */
-		public static void renderWorldLast(DrawHighlightEvent.HighlightBlock event) {
-			RenderSystem.depthMask(false);
-			Vector3d vec = event.getInfo().getPosition();
-			double renderPosX = vec.x();
-			double renderPosY = vec.y();
-			double renderPosZ = vec.z();
-			MatrixStack ms = event.getMatrix();
-			Minecraft mc = Minecraft.getInstance();
-			IRenderTypeBuffer irendertypebuffer1 = mc.renderBuffers().bufferSource();
-			IVertexBuilder vb = irendertypebuffer1.getBuffer(RenderType.lines());
-			ms.pushPose();
-			ms.translate(-renderPosX, -renderPosY, -renderPosZ);
-			Color c = new Color(Utils.properties.bound_color);
-			float red = c.getRed() / 255.0f;
-			float green = c.getGreen() / 255.0f;
-			float blue = c.getBlue() / 255.0f;
-			for (CustomStructureData data : Utils.DATAS_FROM_SERVER) {
-				for (ResourceData rd : data.PIECES_ON_FLY) {
-					MutableBoundingBox mbb = rd.getBounds();
-					WorldRenderer.renderLineBox(ms, vb, mbb.x0, mbb.y0, mbb.z0, mbb.x1, mbb.y1, mbb.z1, red, green,
-							blue, 1.0f);
 				}
 			}
 			ms.popPose();
