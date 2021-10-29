dcecc587d6fa656fd4e7e6bb938a294539b075c8
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2021-10-29 15:50:38 +0200

    Added High Temples Novel

---
+++ src/main/java/com/silverminer/shrines/config/Novels.java
@@ -0,0 +1,17 @@
+package com.silverminer.shrines.config;
+
+public class Novels {
+    public static final String HIGH_TEMPLE = "We have finally made the altar. It is wonderful. I know that they will love it. So will the queen. If we can get more help from them, then we can finally beat “Them”. Who do they think they are, just breaking off and stealing all of our possessions? Luckly, the queen shared with us, so we are not dead. However… (rip here)\n" +
+            "\n" +
+            "the strange coral we found to make the lights is, how do I put it? Quite painful to get. We have to kill all these strange monsters to get it. Oh well, see you tomorrow diary! (Rip)\n" +
+            "\n" +
+            "Oh my! The monsters! We woke up to screaming in the night. Apparently, jon tried to harvest some of the crystals for the queen. Oh, it gets me so mad! He should like ME, not her. But thats not important. What is important is that (Rip)\n" +
+            "\n" +
+            "The monsters cursed us! The whole sea jumped up and swept the crops away! And now the land is cold. There is strange white stuff all over, and I am too cold to go outside. I do hope that the guards can protect us from the raids… Good night. I will write tomorrow.\n" +
+            "\n" +
+            "Oh. I \n" +
+            "have had a dream. It was from the leader of the betrayers! He said that I should join him. I had burst into laughter, but then he said that… He said he knew how to get jon to like me….\n" +
+            "\n" +
+            "How I wish it were true. He had said that If I go to the alter and break the seal, then the cold will stay, but he will enchant jon to love me….\n" +
+            "End of story… For now!";
+}
