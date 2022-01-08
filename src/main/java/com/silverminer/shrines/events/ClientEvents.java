c4324eb1576a8e7fb431d6f4911ec21b0d905b17
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-01-08 17:05:03 +0100

    Fixed #30

--- src/main/java/com/silverminer/shrines/events/ClientEvents.java
+++ src/main/java/com/silverminer/shrines/events/ClientEvents.java
@@ -6,24 +6,24 @@
  */
 package com.silverminer.shrines.events;
 
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.silverminer.shrines.ShrinesMod;
 import com.silverminer.shrines.utils.ClientUtils;
 import com.silverminer.shrines.utils.network.ShrinesPacketHandler;
 import com.silverminer.shrines.utils.network.cts.CTSFetchStructuresPacket;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.WorkingScreen;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ClientEvents {
    protected static final Logger LOGGER = LogManager.getLogger(ClientEvents.class);
@@ -31,11 +31,9 @@ public class ClientEvents {
    @EventBusSubscriber(modid = ShrinesMod.MODID, bus = Bus.FORGE, value = Dist.CLIENT)
    public static class ForgeEventBus {
       @SubscribeEvent
-      public static void onKeyInput(InputEvent.KeyInputEvent event) {
-         int keyCode = event.getKey();
-         int scanCode = event.getScanCode();
+      public static void onClientTick(TickEvent.ClientTickEvent event) {
          Minecraft mc = Minecraft.getInstance();
-         if (ClientUtils.structuresScreen.matches(keyCode, scanCode) && mc.screen == null && mc.player != null) {
+         if (ClientUtils.structuresScreen.isDown()) {
             ShrinesPacketHandler.sendToServer(new CTSFetchStructuresPacket(false));
             mc.setScreen(new WorkingScreen());
          }
@@ -46,11 +44,9 @@ public class ClientEvents {
    public static class ModEventBus {
       @SubscribeEvent
       public static void clientSetupEvent(FMLClientSetupEvent event) {
-         Minecraft mc = event.getMinecraftSupplier().get();
-         KeyBinding[] keyMappings = mc.options.keyMappings;
-         ClientUtils.structuresScreen = new KeyBinding("key.customStructuresScreen", 75, "key.categories.shrines");
-         keyMappings = ArrayUtils.addAll(keyMappings, ClientUtils.structuresScreen);
-         mc.options.keyMappings = keyMappings;
+         ClientUtils.structuresScreen = new KeyBinding("key.customStructuresScreen", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, 75,
+               "key.categories.shrines");
+         ClientRegistry.registerKeyBinding(ClientUtils.structuresScreen);
       }
    }
 }
