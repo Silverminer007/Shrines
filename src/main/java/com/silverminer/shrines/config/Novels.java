9b0dc695447ead765e829db92e2c6275fe9ef6e5
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-01-09 16:19:34 +0100

    Introduced Novels part separator '|' and added new Novel of Shrine of Savanna and Nether Pyramid

--- src/main/java/com/silverminer/shrines/config/Novels.java
+++ src/main/java/com/silverminer/shrines/config/Novels.java
@@ -8,17 +8,33 @@
 package com.silverminer.shrines.config;
 
 public class Novels {
-    public static final String HIGH_TEMPLE = "We have finally made the altar. It is wonderful. I know that they will love it. So will the queen. If we can get more help from them, then we can finally beat “Them”. Who do they think they are, just breaking off and stealing all of our possessions? Luckly, the queen shared with us, so we are not dead. However… (rip here)\n" +
-            "\n" +
-            "the strange coral we found to make the lights is, how do I put it? Quite painful to get. We have to kill all these strange monsters to get it. Oh well, see you tomorrow diary! (Rip)\n" +
-            "\n" +
-            "Oh my! The monsters! We woke up to screaming in the night. Apparently, jon tried to harvest some of the crystals for the queen. Oh, it gets me so mad! He should like ME, not her. But thats not important. What is important is that (Rip)\n" +
-            "\n" +
-            "The monsters cursed us! The whole sea jumped up and swept the crops away! And now the land is cold. There is strange white stuff all over, and I am too cold to go outside. I do hope that the guards can protect us from the raids… Good night. I will write tomorrow.\n" +
-            "\n" +
-            "Oh. I \n" +
-            "have had a dream. It was from the leader of the betrayers! He said that I should join him. I had burst into laughter, but then he said that… He said he knew how to get jon to like me….\n" +
-            "\n" +
-            "How I wish it were true. He had said that If I go to the alter and break the seal, then the cold will stay, but he will enchant jon to love me….\n" +
-            "End of story… For now!";
+   public static final String HIGH_TEMPLE = "We have finally made the altar. It is wonderful. I know that they will love it. So will the queen. If we can get more help from " +
+         "them, then we can finally beat “Them”. Who do they think they are, just breaking off and stealing all of our possessions? Luckily, the queen shared with us, so we are not dead. However… |\n" +
+         "\n" +
+         "the strange coral we found to make the lights is, how do I put it? Quite painful to get. We have to kill all these strange monsters to get it. Oh well, see you " +
+         "tomorrow diary! |\n" +
+         "\n" +
+         "Oh my! The monsters! We woke up to screaming in the night. Apparently, jon tried to harvest some of the crystals for the queen. " +
+         "Oh, it gets me so mad! He should like ME, not her. But that's not important. What is important is that |\n" +
+         "\n" +
+         "The monsters cursed us! The whole sea jumped up and swept the crops away! And now the land is cold. " +
+         "There is strange white stuff all over, and I am too cold to go outside. I do hope that the guards can protect us from the raids… Good night. I will write tomorrow.\n" +
+         "\n" +
+         "Oh. I \n" +
+         "have had a dream. It was from the leader of the betrayers! He said that I should join him. I had burst into laughter, but then he said that… He said he knew how to get jon to like me….\n" +
+         "\n" +
+         "How I wish it were true. He had said that If I go to the alter and break the seal, then the cold will stay, but he will enchant jon to love me…";
+
+   public static final String NETHER_PYRAMID = "It has been done. The sacrifice worked. Now we can get from the temple to the other world. That stupid monk thinks he is such a " +
+         "friend of our group. He will be the next to go to the power. Who knows? Perhaps he\n|" +
+         "will turn into a wither skeleton, or a zombie. I don’t care, as long as he dies. was able to trick him into thinking we are a part of the light fighters. " +
+         "I cannot wait to see his happy face drop, and then bleed out|" +
+         " onto the altar. Soon, however, we will leave this for the other world, and so we must fill it. Fill it with traps made of blood. Solidifying blood is not hard " +
+         "when the helps you. Soon will rule the whole world, and I will be at his side! Now, to take care of that pesky guard at the fort in the other world";
+
+   public static final String SHRINE_OF_SAVANNA = "The sacred place is made. It took many moons to cleanse this hot place, but at last, I can talk yet again with my masters\n|" +
+         " It has been so long since I heard their voices. It is not easy to turn a desert into a forest. But, with my will and hard work, the sky has blessed me, and let me " +
+         "turn this barren wasteland into a lust place,|" +
+         "where all come and let all their troubles melt away. {Ink spill} Oh my, they want me back?|" +
+         "Oh happy day! I will leave and go home! I know that this place will help any weary travelers who find it by the gods will. If you read this, Feel free to take what you need";
 }
