6624afe7ced25eaee2ade60436d629b8b0c39326
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-01-24 16:32:20 +0100

    BETA 3.0.0-Beta9

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Shrines
+
 ### This is a Structure Generation Mod for Minecraft/MinecraftForge
+
 Detailed information about the structure and the mod you can find on curseforge and discord:
+
 - https://www.curseforge.com/minecraft/mc-mods/shrines-structures
 - https://discord.gg/8pUpWCEUe2
 - [Wiki](https://github.com/Silverminer007/MinecraftModsUpdateChecker/blob/master/wiki/SelectLanguage.md)
+
+### TODO
+
+- [ ] Fix warning 'Potentially Dangerous alternative prefix `shrines` for name `trader_house`, expected `minecraft`. This could be a intended override, but in most cases indicates
+  a broken mod.'
+- [ ] Fix Terraforged compactability warning 'Was unable to check if minecraft:overworld is using Terraforged's ChunkGenerator.'
--- src/main/java/com/silverminer/shrines/init/NovelsRegistry.java
+++ src/main/java/com/silverminer/shrines/init/NovelsRegistry.java
@@ -29,7 +29,7 @@ public class NovelsRegistry {
 
                Oh. I
                have had a dream. It was from the leader of the betrayers! He said that I should join him. I had burst into laughter, but then he said that… He said he knew how to get jon to like me…
-               
+                              
                How I wish it were true. He had said that If I go to the alter and break the seal, then the cold will stay, but he will enchant jon to love me…"""));
    public static final RegistryObject<StructureNovel> NETHER_PYRAMID = NOVELS.register("nether_pyramid", () -> new StructureNovel(
          "It has been done. The sacrifice worked. Now we can get from the temple to the other world. That stupid monk thinks he is such a friend of our group. He will be the next to go to the power. Who knows? Perhaps he\n",
@@ -40,4 +40,31 @@ public class NovelsRegistry {
          " It has been so long since I heard their voices. It is not easy to turn a desert into a forest. But, with my will and hard work, the sky has blessed me, and let me turn this barren wasteland into a lust place,",
          "where all come and let all their troubles melt away. {Ink spill} Oh my, they want me back?",
          "Oh happy day! I will leave and go home! I know that this place will help any weary travelers who find it by the gods will. If you read this, Feel free to take what you need"));
+   public static final RegistryObject<StructureNovel> MINERAL_TEMPLE = NOVELS.register("mineral_temple", () -> new StructureNovel("The hideout is almost complete. ᒲ⊣ᔑリℸ ̣ ⍑ Is " +
+         "in desperate need of a hide out, and my own home would not suffice. Therefore, I am making this rock. She will be able to farm the moss, smear it with blood, and " +
+         "become fully invisible. Well, the house will be invisible. She will still be visible if she leaves. However, if we ca",
+         "N stop the progress of that nun and stupid monk, we may be able to win  ̵̿̚ ̶̉̍ ̷̔́ ̸́̍ ̸̛̑ ̴̔̊ ̶̿̈ ̸́̔ ̷̃́ ̴̽͂ ̵̍̅ ̷̾͠ ̶͒͠ ̴͐́ ̷̀̓ ̸̆̕ ̵̑̀ ̶̍͝ ̷̈́͊ ̴͑̚ ̸͑̌ ̸̀͋ ̶̊͝ " +
+               "̸̓͊", "Of course, the monk would not be stupid, like her. Perhaps… Perhaps making this monster is not the best idea. However, I cannot stop now. Someone must " +
+         "win this. And I will be the one to make the winning move, with that witches help"));
+   public static final RegistryObject<StructureNovel> SMALL_TEMPLE = NOVELS.register("small_temple", () -> new StructureNovel("I have found a new family in the forest. The " +
+         "woman is going to have a child. I must stay away from them. The last thing I need is for them to alert the gods where I am. It was an ingenious idea to hide in their " +
+         "own temple, but I cannot be found until They come.",
+         "The woman thinks this is my house. She is sick and going to die. That idiotic man thinks that I will help deliver her child. I have no choice. I must play along. I " +
+               "need a plan. I do have an idea. Please reply to me, ᔑ⎓⋮ᓭ⎓╎⋮∴ʖ╎⍊ʖ╎∴ʖ∷⍊∷∴ʖ⍊. I need your help.",
+         "It has been done. The snowstorm spell you sent me worked perfectly. The man wonders where his wife is, but I will have him dead by tonight. As I promised, their souls" +
+               " will be yours. As of now, I plan to leave this place. Thank you for your help ᔑ⎓⋮ᓭ⎓╎⋮∴ʖ╎⍊ʖ╎∴ʖ∷⍊∷∴ʖ⍊. \n" +
+               "\n" +
+               "Signed,\n" +
+               "ᒲ⊣ᔑリℸ ̣ ⍑"));
+   public static final RegistryObject<StructureNovel> WATER_SHRINE = NOVELS.register("water_shrine", () -> new StructureNovel("The spring has been found. We needed this water " +
+         "badly. We " +
+         "cannot keep burning our tinder just to make water. My wife thinks that this will be a good place to raise a baby, but I cannot help but miss the warmth of my desert " +
+         "home.",
+         "\nYou will need to search for further parts in other structures! Here is nothing left!"));
+   public static final RegistryObject<StructureNovel> PLAYER_HOUSE = NOVELS.register("player_house", () -> new StructureNovel("The house is somewhat finished. I can’t wait to " +
+         "get out of " +
+         "this hellish cold! I have been working nonstop on the roof, but every time I get close to sealing the roof, it collapses! Yet another problem with snow. My wife is " +
+         "going to have her baby soon, and so she is in a house nearby right now with an old lady. Hopefully I will be done with this house by next week.",
+         "I can barely lift my hand to write now. The blizzard and the fever have sapped all the life out of me. I am going to try and find my wife tomorrow. She disappeared, " +
+               "and the old lady said she went to meet me. I am not sure I trust this woman anymore. I- [blood and scribbles from here before]"));
 }
