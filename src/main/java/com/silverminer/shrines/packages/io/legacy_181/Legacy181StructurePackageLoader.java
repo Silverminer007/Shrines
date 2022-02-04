f466dc55e6c50504a692d484128d31602a35392a
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-02-04 17:32:54 +0100

    Improved Legacy 1.8.1 Structure Configuration Import - Use default values and override options if they're available - Don't stop import when import of one structure fails - Convert dimension 'end' to 'the_end' and 'nether' to 'the_nether'

--- src/main/java/com/silverminer/shrines/packages/io/legacy_181/Legacy181StructurePackageLoader.java
+++ src/main/java/com/silverminer/shrines/packages/io/legacy_181/Legacy181StructurePackageLoader.java
@@ -19,6 +19,8 @@ import com.silverminer.shrines.packages.io.legacy_181.configuration.LegacyStruct
 import com.silverminer.shrines.utils.CalculationError;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -31,6 +33,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Legacy181StructurePackageLoader implements StructurePackageLoader {
+   protected static final Logger LOGGER = LogManager.getLogger(Legacy181StructurePackageLoader.class);
    private final Legacy181DirectoryStructureAccessor directoryStructureAccessor = new Legacy181DirectoryStructureAccessor();
 
    @Override
@@ -109,43 +112,82 @@ public class Legacy181StructurePackageLoader implements StructurePackageLoader {
       return new StructuresPackageWrapper(structuresPackageInfo, structures, new StructureTemplateContainer(), new TemplatePoolContainer());
    }
 
-   private StructuresPackageWrapper loadLegacyIncludedStructures() throws PackageIOException {
+   private StructuresPackageWrapper loadLegacyIncludedStructures() {
       Path configPath = FMLPaths.CONFIGDIR.get().resolve("shrines-server.toml");
       CommentedFileConfig configData = CommentedFileConfig.of(configPath);
       configData.load();
-      StructureDataContainer structureDataContainer = new StructureDataContainer();
-      try {
-         for (String key : Arrays.asList("abandoned_witch_house", "ballon", "bees", "end_temple", "flooded_temple", "guardian_meeting", "harbour", "high_tempel", "infested_prison",
-               "jungle_tower", "mineral_temple", "nether_pyramid", "nether_shrine", "oriental_sanctuary", "player_house", "small_tempel", "water_shrine"
-         )) {
-            ResourceLocation keyLocation = new ResourceLocation(ShrinesMod.MODID, key);
-            String name = keyLocation.toString();
-            boolean transformLand = configData.get(Arrays.asList("structures", key, "needs_ground"));
-            boolean generate = configData.get(Arrays.asList("structures", key, "generate"));
-            double spawnChance = configData.get(Arrays.asList("structures", key, "spawn_chance"));
-            int distance = configData.get(Arrays.asList("structures", key, "distance"));
-            int separation = configData.get(Arrays.asList("structures", key, "seperation"));
-            int seedModifier = configData.get(Arrays.asList("structures", key, "seed"));
-            int heightOffset = 0;
-            List<String> biomeBlacklist = configData.get(Arrays.asList("structures", key, "blacklist"));
-            List<String> biomeCategoryWhitelist = configData.get(Arrays.asList("structures", key, "categories"));
-            List<String> dimensionWhitelist = configData.get(Arrays.asList("structures", key, "dimensions"));
-            String startPool = "";
-            int jigsawMaxDepth = 7;
-            SpawnConfiguration spawnConfiguration = new SpawnConfiguration(transformLand, generate, spawnChance, distance, separation, seedModifier, heightOffset, biomeBlacklist,
-                  biomeCategoryWhitelist, dimensionWhitelist, startPool, jigsawMaxDepth);
-            VariationConfiguration variationConfiguration = configData.get(Arrays.asList("structures", key, "use_random_varianting")) ? VariationConfiguration.ALL_ENABLED :
-                  VariationConfiguration.ALL_DISABLED;
-            structureDataContainer.add(new StructureData(name, keyLocation, spawnConfiguration, null, null, variationConfiguration));
+      StructureDataContainer structureDataContainer = this.getLegacyIncludedStructures();
+      for (StructureData structureData : structureDataContainer.getAsIterable()) {
+         try {
+            String key = structureData.getKey().getPath();
+            SpawnConfiguration spawnConfiguration = structureData.getSpawnConfiguration();
+            spawnConfiguration.setTransformLand(configData.get(Arrays.asList("structures", key, "needs_ground")));
+            spawnConfiguration.setGenerate(configData.get(Arrays.asList("structures", key, "generate")));
+            spawnConfiguration.setSpawn_chance(configData.get(Arrays.asList("structures", key, "spawn_chance")));
+            spawnConfiguration.setDistance(configData.get(Arrays.asList("structures", key, "distance")));
+            spawnConfiguration.setSeparation(configData.get(Arrays.asList("structures", key, "seperation")));
+            spawnConfiguration.setSeed_modifier(configData.get(Arrays.asList("structures", key, "seed")));
+            spawnConfiguration.setBiome_blacklist(configData.get(Arrays.asList("structures", key, "blacklist")));
+            spawnConfiguration.setBiome_category_whitelist(configData.get(Arrays.asList("structures", key, "categories")));
+            spawnConfiguration.setDimension_whitelist(configData.get(Arrays.asList("structures", key, "dimensions")));
+            List<String> dimensionWhitelist = spawnConfiguration.getDimension_whitelist();
+            if (dimensionWhitelist.contains("end")) {
+               dimensionWhitelist.remove("end");
+               dimensionWhitelist.add("the_end");
+            }
+            if (dimensionWhitelist.contains("nether")) {
+               dimensionWhitelist.remove("nether");
+               dimensionWhitelist.add("the_nether");
+            }
+            boolean randomVariation = configData.get(Arrays.asList("structures", key, "use_random_varianting"));
+            VariationConfiguration variationConfiguration = structureData.getVariationConfiguration();
+            variationConfiguration.setEnabled(randomVariation);
+            variationConfiguration.setSimpleVariationConfiguration(randomVariation ? SimpleVariationConfiguration.ALL_ENABLED : SimpleVariationConfiguration.ALL_DISABLED);
+            variationConfiguration.setNestedVariationConfiguration(randomVariation ? NestedVariationConfiguration.ALL_ENABLED : NestedVariationConfiguration.ALL_DISABLED);
+         } catch (NullPointerException e) {
+            if (!structureData.getKey().getNamespace().equals("ballon")
+                  && !structureData.getKey().getNamespace().equals("small_tempel")
+                  && !structureData.getKey().getNamespace().equals("high_tempel")) {
+               LOGGER.error("Failed to import legacy structure config from shrines-server.toml for structure [{}]", structureData.getKey());
+            }
          }
-      } catch (NullPointerException e) {
-         throw new PackageIOException(new CalculationError("Failed to load structure previously saved in 1.8.1", "Config format is invalid (shrines-server.toml)"));
       }
-      structureDataContainer.add(DefaultStructureConfig.SHRINEOFSAVANNA_CONFIG.toStructureData());
-      structureDataContainer.add(DefaultStructureConfig.TRADER_HOUSE_CONFIG.toStructureData());
-      structureDataContainer.add(DefaultStructureConfig.WATCHTOWER_CONFIG.toStructureData());
       StructuresPackageInfo structuresPackageInfo = new StructuresPackageInfo("Included Structures (Legacy Config)", "Silverm7ner");
       return new StructuresPackageWrapper(structuresPackageInfo, structureDataContainer, new StructureTemplateContainer(), new TemplatePoolContainer());
+   }
+
+   /**
+    * @return all structures that are available by default in 2.x.x but with legacy (1.8.1) structure keys
+    */
+   private StructureDataContainer getLegacyIncludedStructures() {
+      StructureDataContainer structures = new StructureDataContainer();
+      structures.add(DefaultStructureConfig.ABANDONEDWITCHHOUSE_CONFIG.toStructureData());
+      StructureData balloonData = DefaultStructureConfig.BALLON_CONFIG.toStructureData();
+      balloonData.setKey(new ResourceLocation(ShrinesMod.MODID, "ballon"));
+      structures.add(balloonData);
+      structures.add(DefaultStructureConfig.BEES_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.ENDTEMPLE_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.FLOODEDTEMPLE_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.GUARDIANMEETING_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.HARBOUR_CONFIG.toStructureData());
+      StructureData highTempleData = DefaultStructureConfig.HIGHTEMPLE_CONFIG.toStructureData();
+      highTempleData.setKey(new ResourceLocation(ShrinesMod.MODID, "high_tempel"));
+      structures.add(highTempleData);
+      structures.add(DefaultStructureConfig.INFESTEDPRISON_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.JUNGLETOWER_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.MINERALTEMPLE_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.NETHERPYRAMID_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.NETHERSHRINE_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.ORIENTALSANCTUARY_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.PLAYERHOUSE_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.SHRINEOFSAVANNA_CONFIG.toStructureData());
+      StructureData smallTempleData = DefaultStructureConfig.SMALLTEMPLE_CONFIG.toStructureData();
+      smallTempleData.setKey(new ResourceLocation(ShrinesMod.MODID, "small_tempel"));
+      structures.add(smallTempleData);
+      structures.add(DefaultStructureConfig.TRADER_HOUSE_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.WATCHTOWER_CONFIG.toStructureData());
+      structures.add(DefaultStructureConfig.WATERSHRINE_CONFIG.toStructureData());
+      return structures;
    }
 
    @Override
