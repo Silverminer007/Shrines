3e95edeb2f5ea1802d5100053ffa877626fca025
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-02-07 16:35:50 +0100

    Updated Structure Config UI to use new Variation configuration

--- src/main/java/com/silverminer/shrines/packages/server/ServerStructurePackageManager.java
+++ src/main/java/com/silverminer/shrines/packages/server/ServerStructurePackageManager.java
@@ -8,6 +8,7 @@
 package com.silverminer.shrines.packages.server;
 
 import com.silverminer.shrines.init.NovelsRegistry;
+import com.silverminer.shrines.init.VariationMaterialsRegistry;
 import com.silverminer.shrines.packages.container.NovelDataContainer;
 import com.silverminer.shrines.packages.container.StructurePackageContainer;
 import com.silverminer.shrines.packages.datacontainer.StructureNovel;
@@ -18,6 +19,8 @@ import com.silverminer.shrines.utils.CalculationError;
 import com.silverminer.shrines.utils.network.ShrinesPacketHandler;
 import com.silverminer.shrines.utils.network.stc.*;
 import com.silverminer.shrines.utils.queue.PlayerQueue;
+import com.silverminer.shrines.worldgen.structures.variation.NewVariationMaterial;
+import com.silverminer.shrines.worldgen.structures.variation.NewVariationMaterialElement;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -26,168 +29,178 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class ServerStructurePackageManager {
-   protected static final Logger LOGGER = LogManager.getLogger(ServerStructurePackageManager.class);
-   protected final StructurePackageIOManager ioManager = StructurePackageIOManager.INSTANCE;
-   protected final PlayerQueue playerQueue = new PlayerQueue();
-   protected StructurePackageContainer packages = new StructurePackageContainer();
-   protected StructurePackageContainer initialStructurePackages;
+    protected static final Logger LOGGER = LogManager.getLogger(ServerStructurePackageManager.class);
+    protected final StructurePackageIOManager ioManager = StructurePackageIOManager.INSTANCE;
+    protected final PlayerQueue playerQueue = new PlayerQueue();
+    protected StructurePackageContainer packages = new StructurePackageContainer();
+    protected StructurePackageContainer initialStructurePackages;
 
-   public StructurePackageContainer getInitialStructurePackages() {
-      return initialStructurePackages;
-   }
+    public StructurePackageContainer getInitialStructurePackages() {
+        return initialStructurePackages;
+    }
 
-   public void bootstrapPackages() {
-      if (this.getPackages() == null || this.getPackages().getSize() == 0) {
-         this.earlyLoadPackages();
-      }
-      this.initialStructurePackages = this.getPackages();
-   }
+    public void bootstrapPackages() {
+        if (this.getPackages() == null || this.getPackages().getSize() == 0) {
+            this.earlyLoadPackages();
+        }
+        this.initialStructurePackages = this.getPackages();
+    }
 
-   public StructurePackageContainer getPackages() {
-      return this.packages;
-   }
+    public StructurePackageContainer getPackages() {
+        return this.packages;
+    }
 
-   public void earlyLoadPackages() {
-      try {
-         this.packages = this.ioManager.earlyLoadPackages();
-      } catch (PackageIOException e) {
-         this.onError(new CalculationError("Failed to load packages", "Caused by: %s", e.getReason()));
-         e.printStackTrace();
-      }
-   }
+    public void earlyLoadPackages() {
+        try {
+            this.packages = this.ioManager.earlyLoadPackages();
+        } catch (PackageIOException e) {
+            this.onError(new CalculationError("Failed to load packages", "Caused by: %s", e.getReason()));
+            e.printStackTrace();
+        }
+    }
 
-   public void onError(CalculationError error) {
-      if (this.playerQueue.getQueue().size() > 0 && this.playerQueue.getQueue().get(0) != null) {
-         this.onError(error, this.playerQueue.getQueue().get(0));
-      } else {
-         this.onError(error, new UUID[0]);
-      }
-   }
+    public void onError(CalculationError error) {
+        if (this.playerQueue.getQueue().size() > 0 && this.playerQueue.getQueue().get(0) != null) {
+            this.onError(error, this.playerQueue.getQueue().get(0));
+        } else {
+            this.onError(error, new UUID[0]);
+        }
+    }
 
-   public void onError(CalculationError error, UUID... clientIDs) {
-      for (UUID client : clientIDs) {
-         ShrinesPacketHandler.sendTo(new STCError(error), client, false);
-      }
-      LOGGER.error(error);
-   }
+    public void onError(CalculationError error, UUID... clientIDs) {
+        for (UUID client : clientIDs) {
+            ShrinesPacketHandler.sendTo(new STCError(error), client, false);
+        }
+        LOGGER.error(error);
+    }
 
-   public void onPlayerJoinQueue(UUID playerID) {
-      this.playerQueue.join(playerID);
-   }
+    public void onPlayerJoinQueue(UUID playerID) {
+        this.playerQueue.join(playerID);
+    }
 
-   public void onPlayerLeaveQueue(UUID playerID) {
-      this.playerQueue.leave(playerID);
-   }
+    public void onPlayerLeaveQueue(UUID playerID) {
+        this.playerQueue.leave(playerID);
+    }
 
-   public void syncInitialPackagesToClient(UUID playerID) {
-      ShrinesPacketHandler.sendTo(new STCSyncInitialPackages(this.getPackages()), playerID);
-   }
+    public void syncInitialPackagesToClient(UUID playerID) {
+        ShrinesPacketHandler.sendTo(new STCSyncInitialPackages(this.getPackages()), playerID);
+    }
 
-   public void syncAvailableDimensionsToClient(UUID uuid) {
-      List<String> availableDimensions = ServerLifecycleHooks.getCurrentServer().levelKeys().stream().map(ResourceKey::location).map(ResourceLocation::toString).toList();
-      ShrinesPacketHandler.sendTo(new STCSyncAvailableDimensions(availableDimensions), uuid);
-   }
+    public void syncAvailableDimensionsToClient(UUID uuid) {
+        List<String> availableDimensions = ServerLifecycleHooks.getCurrentServer().levelKeys().stream().map(ResourceKey::location).map(ResourceLocation::toString).toList();
+        ShrinesPacketHandler.sendTo(new STCSyncAvailableDimensions(availableDimensions), uuid);
+    }
 
-   public void syncNovelsToClient(UUID uuid) {
-      Map<ResourceLocation, StructureNovel> novelsRegistryData = new HashMap<>();
-      for (Map.Entry<ResourceKey<StructureNovel>, StructureNovel> entry : NovelsRegistry.NOVELS_REGISTRY.get().getEntries()) {
-         novelsRegistryData.put(entry.getKey().location(), entry.getValue());
-      }
-      ShrinesPacketHandler.sendTo(new STCSyncNovels(this.getNovels(uuid), novelsRegistryData), uuid);
-   }
-
-   public NovelDataContainer getNovels(UUID playerID) {
-      return NovelsDataRegistry.INSTANCE.getNovelsData(playerID);
-   }
-
-   public void syncStructureIconsToClient(UUID uuid) {
-      try {
-         ShrinesPacketHandler.sendTo(new STCSyncStructureIcons(this.ioManager.loadStructureIcons(this.getPackages())), uuid);
-      } catch (PackageIOException e) {
-         this.onError(e.getReason());
-      }
-   }
-
-   public void clientSavedPackages(StructurePackageContainer packageContainer, UUID clientID) {
-      if (this.playerIDIsValid(clientID) && this.playerHasPermission(clientID, 2)) {
-         this.packages = packageContainer;
-         boolean success = this.savePackages();
-         ShrinesPacketHandler.sendTo(new STCSendSaveResult(success), clientID);
-      } else {
-         this.onError(CalculationError.PLAYER_MISSING_PERMISSION);
-      }
-   }
-
-   public boolean savePackages() {
-      try {
-         this.ioManager.savePackages(this.packages);
-         return true;
-      } catch (PackageIOException e) {
-         this.onError(new CalculationError("Failed to save packages", "Caused by: %s", e.getReason()));
-         e.printStackTrace();
-         return false;
-      }
-   }
-
-   public void importPackage(byte[] packageFile, @Nullable UUID playerID) {
-      try {
-         if (this.playerIDIsValid(playerID) && this.playerHasPermission(playerID, 2)) {
-            StructuresPackageWrapper structuresPackageWrapper = this.ioManager.importPackage(packageFile);
-            this.getPackages().add(structuresPackageWrapper);
-            this.syncPackagesToClient(playerID);
-         } else {
-            this.onError(CalculationError.PLAYER_MISSING_PERMISSION);
-         }
-      } catch (PackageIOException e) {
-         this.onError(new CalculationError("Failed to import package", "Caused by: %s", e));
-      }
-   }
-
-   private boolean playerIDIsValid(@Nullable UUID playerID) {
-      return this.playerQueue.getQueue().size() > 0 && this.playerQueue.getQueue().get(0).equals(playerID);
-   }
-
-   private boolean playerHasPermission(UUID playerID, int permissionLevel) {
-      ServerPlayer player = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayer(playerID);
-      return player != null && player.hasPermissions(permissionLevel);
-   }
-
-   public void syncPackagesToClient(UUID playerID) {
-      if (this.playerIDIsValid(playerID)) {
-         ShrinesPacketHandler.sendTo(new STCSyncPackages(this.getPackages()), playerID);
-      } else {
-         this.onError(new CalculationError("Received invalid save request from client", "Got package save request from client that hadn't had the permission to do that"), playerID);
-      }
-   }
-
-   public void exportPackage(UUID packageID, @Nullable UUID playerID) {
-      try {
-         if (this.playerIDIsValid(playerID) && this.playerHasPermission(playerID, 1)) {
-            StructuresPackageWrapper structuresPackageWrapper = this.getPackages().getByKey(packageID);
-            if (structuresPackageWrapper != null) {
-               byte[] packageFile = this.ioManager.exportPackage(structuresPackageWrapper);
-               ShrinesPacketHandler.sendTo(new STCSaveExportedPackage(packageFile), playerID);
+    public void syncAvailableMaterialsATypesToClient(UUID uuid) {
+        List<String> availableMaterials = VariationMaterialsRegistry.VARIATION_MATERIALS_REGISTRY.get().getValues().stream().map(NewVariationMaterial::materialID).distinct().toList();
+        List<String> availableTypes = new ArrayList<>();
+        for (NewVariationMaterial material : VariationMaterialsRegistry.VARIATION_MATERIALS_REGISTRY.get().getValues()) {
+            for (NewVariationMaterialElement element : material.types()) {
+                if (!availableTypes.contains(element.typeID())) {
+                    availableTypes.add(element.typeID());
+                }
             }
-         } else {
-            this.onError(CalculationError.PLAYER_MISSING_PERMISSION);
-         }
-      } catch (PackageIOException e) {
-         this.onError(new CalculationError("Failed to export package", "Caused by: %s", e));
-      }
-   }
+        }
+        ShrinesPacketHandler.sendTo(new STCSyncAvailableMaterialsATypes(availableMaterials, availableTypes), uuid);
+    }
 
-   public void loadPackages() {
-      try {
-         this.packages = this.ioManager.loadPackages();
-      } catch (PackageIOException e) {
-         this.onError(new CalculationError("Failed to load packages", "Caused by: %s", e.getReason()));
-         e.printStackTrace();
-      }
-   }
+    public void syncNovelsToClient(UUID uuid) {
+        Map<ResourceLocation, StructureNovel> novelsRegistryData = new HashMap<>();
+        for (Map.Entry<ResourceKey<StructureNovel>, StructureNovel> entry : NovelsRegistry.NOVELS_REGISTRY.get().getEntries()) {
+            novelsRegistryData.put(entry.getKey().location(), entry.getValue());
+        }
+        ShrinesPacketHandler.sendTo(new STCSyncNovels(this.getNovels(uuid), novelsRegistryData), uuid);
+    }
+
+    public NovelDataContainer getNovels(UUID playerID) {
+        return NovelsDataRegistry.INSTANCE.getNovelsData(playerID);
+    }
+
+    public void syncStructureIconsToClient(UUID uuid) {
+        try {
+            ShrinesPacketHandler.sendTo(new STCSyncStructureIcons(this.ioManager.loadStructureIcons(this.getPackages())), uuid);
+        } catch (PackageIOException e) {
+            this.onError(e.getReason());
+        }
+    }
+
+    public void clientSavedPackages(StructurePackageContainer packageContainer, UUID clientID) {
+        if (this.playerIDIsValid(clientID) && this.playerHasPermission(clientID, 2)) {
+            this.packages = packageContainer;
+            boolean success = this.savePackages();
+            ShrinesPacketHandler.sendTo(new STCSendSaveResult(success), clientID);
+        } else {
+            this.onError(CalculationError.PLAYER_MISSING_PERMISSION);
+        }
+    }
+
+    public boolean savePackages() {
+        try {
+            this.ioManager.savePackages(this.packages);
+            return true;
+        } catch (PackageIOException e) {
+            this.onError(new CalculationError("Failed to save packages", "Caused by: %s", e.getReason()));
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void importPackage(byte[] packageFile, @Nullable UUID playerID) {
+        try {
+            if (this.playerIDIsValid(playerID) && this.playerHasPermission(playerID, 2)) {
+                StructuresPackageWrapper structuresPackageWrapper = this.ioManager.importPackage(packageFile);
+                this.getPackages().add(structuresPackageWrapper);
+                this.syncPackagesToClient(playerID);
+            } else {
+                this.onError(CalculationError.PLAYER_MISSING_PERMISSION);
+            }
+        } catch (PackageIOException e) {
+            this.onError(new CalculationError("Failed to import package", "Caused by: %s", e));
+        }
+    }
+
+    private boolean playerIDIsValid(@Nullable UUID playerID) {
+        return this.playerQueue.getQueue().size() > 0 && this.playerQueue.getQueue().get(0).equals(playerID);
+    }
+
+    private boolean playerHasPermission(UUID playerID, int permissionLevel) {
+        ServerPlayer player = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayer(playerID);
+        return player != null && player.hasPermissions(permissionLevel);
+    }
+
+    public void syncPackagesToClient(UUID playerID) {
+        if (this.playerIDIsValid(playerID)) {
+            ShrinesPacketHandler.sendTo(new STCSyncPackages(this.getPackages()), playerID);
+        } else {
+            this.onError(new CalculationError("Received invalid save request from client", "Got package save request from client that hadn't had the permission to do that"), playerID);
+        }
+    }
+
+    public void exportPackage(UUID packageID, @Nullable UUID playerID) {
+        try {
+            if (this.playerIDIsValid(playerID) && this.playerHasPermission(playerID, 1)) {
+                StructuresPackageWrapper structuresPackageWrapper = this.getPackages().getByKey(packageID);
+                if (structuresPackageWrapper != null) {
+                    byte[] packageFile = this.ioManager.exportPackage(structuresPackageWrapper);
+                    ShrinesPacketHandler.sendTo(new STCSaveExportedPackage(packageFile), playerID);
+                }
+            } else {
+                this.onError(CalculationError.PLAYER_MISSING_PERMISSION);
+            }
+        } catch (PackageIOException e) {
+            this.onError(new CalculationError("Failed to export package", "Caused by: %s", e));
+        }
+    }
+
+    public void loadPackages() {
+        try {
+            this.packages = this.ioManager.loadPackages();
+        } catch (PackageIOException e) {
+            this.onError(new CalculationError("Failed to load packages", "Caused by: %s", e.getReason()));
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/silverminer/shrines/utils/network/stc/STCSyncAvailableMaterialsATypes.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.silverminer.shrines.utils.network.stc;
+
+import com.silverminer.shrines.packages.PackageManagerProvider;
+import com.silverminer.shrines.utils.network.IPacket;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class STCSyncAvailableMaterialsATypes implements IPacket {
+    private final List<String> availableMaterials;
+    private final List<String> availableTypes;
+
+    public STCSyncAvailableMaterialsATypes(List<String> availableDimensions, List<String> availableTypes) {
+        this.availableMaterials = availableDimensions;
+        this.availableTypes = availableTypes;
+    }
+
+    public STCSyncAvailableMaterialsATypes(FriendlyByteBuf buf) {
+        CompoundTag tag = buf.readNbt();
+        if (tag == null) {
+            this.availableMaterials = new ArrayList<>();
+        } else {
+            ListTag dimensions = tag.getList("materials", 8);// Type 8 is StringTag
+            this.availableMaterials = dimensions.stream().filter(dimension -> dimension instanceof StringTag).map(Tag::getAsString).collect(Collectors.toList());
+        }
+        if (tag == null) {
+            this.availableTypes = new ArrayList<>();
+        } else {
+            ListTag dimensions = tag.getList("types", 8);// Type 8 is StringTag
+            this.availableTypes = dimensions.stream().filter(dimension -> dimension instanceof StringTag).map(Tag::getAsString).collect(Collectors.toList());
+        }
+    }
+
+    public void toBytes(FriendlyByteBuf buf) {
+        CompoundTag tag = new CompoundTag();
+        ListTag materials = new ListTag();
+        materials.addAll(this.availableMaterials.stream().map(StringTag::valueOf).toList());
+        tag.put("materials", materials);
+        ListTag types = new ListTag();
+        types.addAll(this.availableTypes.stream().map(StringTag::valueOf).toList());
+        tag.put("types", types);
+        buf.writeNbt(tag);
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() -> {
+            PackageManagerProvider.CLIENT.setAvailableMaterials(this.availableMaterials);
+            PackageManagerProvider.CLIENT.setAvailableTypes(this.availableTypes);
+        });
+        ctx.get().setPacketHandled(true);
+    }
+}
