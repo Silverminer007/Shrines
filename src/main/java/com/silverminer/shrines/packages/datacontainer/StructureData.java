35c515ec5fbc4e04678eab5257f4eb56e5f2b544
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-02-07 15:58:18 +0100

    Fixed configuration upgrade

--- src/main/java/com/silverminer/shrines/packages/datacontainer/StructureData.java
+++ src/main/java/com/silverminer/shrines/packages/datacontainer/StructureData.java
@@ -30,125 +30,125 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class StructureData implements Comparable<StructureData> {
-   public static final Codec<StructureData> CODEC = RecordCodecBuilder.create((structureDataInstance) -> structureDataInstance.group(
-               Codec.STRING.fieldOf(ConfigOptions.LATEST.name()).forGetter(StructureData::getName),
-               ResourceLocation.CODEC.fieldOf(ConfigOptions.LATEST.key()).forGetter(StructureData::getKey),
-               SpawnConfiguration.CODEC.fieldOf("spawn_configuration").forGetter(StructureData::getSpawnConfiguration),
-               ResourceLocation.CODEC.optionalFieldOf(ConfigOptions.LATEST.iconPath(), null).forGetter(StructureData::getIconPath),
-               ResourceLocation.CODEC.optionalFieldOf(ConfigOptions.LATEST.novel(), new ResourceLocation("")).forGetter(StructureData::getNovel),
-               Codec.either(VariationConfiguration.CODEC, NewVariationConfiguration.CODEC).optionalFieldOf("variation_configuration",
-                     Either.left(VariationConfiguration.ALL_DISABLED)).forGetter(structureData -> Either.right(structureData.variationConfiguration)))
-         .apply(structureDataInstance, StructureData::new));
-   protected static final Logger LOGGER = LogManager.getLogger(StructureData.class);
-   private final SpawnConfiguration spawnConfiguration;
-   private NewVariationConfiguration variationConfiguration;
-   private String name;
-   private ResourceLocation key;
-   private ResourceLocation novel;
-   private ResourceLocation iconPath;
+    public static final Codec<StructureData> CODEC = RecordCodecBuilder.create((structureDataInstance) -> structureDataInstance.group(
+                    Codec.STRING.fieldOf(ConfigOptions.LATEST.name()).forGetter(StructureData::getName),
+                    ResourceLocation.CODEC.fieldOf(ConfigOptions.LATEST.key()).forGetter(StructureData::getKey),
+                    SpawnConfiguration.CODEC.fieldOf("spawn_configuration").forGetter(StructureData::getSpawnConfiguration),
+                    ResourceLocation.CODEC.optionalFieldOf(ConfigOptions.LATEST.iconPath(), null).forGetter(StructureData::getIconPath),
+                    ResourceLocation.CODEC.optionalFieldOf(ConfigOptions.LATEST.novel(), new ResourceLocation("")).forGetter(StructureData::getNovel),
+                    Codec.either(VariationConfiguration.CODEC, NewVariationConfiguration.CODEC).optionalFieldOf("variation_configuration",
+                            Either.right(new NewVariationConfiguration(false))).forGetter(structureData -> Either.right(structureData.variationConfiguration)))
+            .apply(structureDataInstance, StructureData::new));
+    protected static final Logger LOGGER = LogManager.getLogger(StructureData.class);
+    private final SpawnConfiguration spawnConfiguration;
+    private NewVariationConfiguration variationConfiguration;
+    private String name;
+    private ResourceLocation key;
+    private ResourceLocation novel;
+    private ResourceLocation iconPath;
 
-   public StructureData(String name, ResourceLocation key, SpawnConfiguration spawnConfiguration, @Nullable ResourceLocation iconPath, @Nullable ResourceLocation novel) {
-      this(name, key, spawnConfiguration, iconPath, novel, Either.right(new NewVariationConfiguration(false)));
-   }
+    public StructureData(String name, ResourceLocation key, SpawnConfiguration spawnConfiguration, @Nullable ResourceLocation iconPath, @Nullable ResourceLocation novel) {
+        this(name, key, spawnConfiguration, iconPath, novel, Either.right(new NewVariationConfiguration(false)));
+    }
 
-   public StructureData(String name, ResourceLocation key, SpawnConfiguration spawnConfiguration, @Nullable ResourceLocation iconPath, @Nullable ResourceLocation novel,
-                        NewVariationConfiguration variationConfiguration) {
-      this(name, key, spawnConfiguration, iconPath, novel, Either.right(variationConfiguration));
-   }
+    public StructureData(String name, ResourceLocation key, SpawnConfiguration spawnConfiguration, @Nullable ResourceLocation iconPath, @Nullable ResourceLocation novel,
+                         NewVariationConfiguration variationConfiguration) {
+        this(name, key, spawnConfiguration, iconPath, novel, Either.right(variationConfiguration));
+    }
 
-   /**
-    * @param name               the Displayname for the Structure
-    * @param key                the Key of the structure. This identifies this structure
-    * @param spawnConfiguration spawn configuration options of this structure
-    * @param iconPath           the path to the icon of this structure or null to use key's value
-    * @param novel              the path to the structure's Novels or null to use key's value
-    */
-   public StructureData(String name, ResourceLocation key, SpawnConfiguration spawnConfiguration, @Nullable ResourceLocation iconPath, @Nullable ResourceLocation novel,
-                        Either<VariationConfiguration, NewVariationConfiguration> variationConfiguration) {
-      this.name = name;
-      this.key = key;
-      this.novel = Objects.requireNonNullElse(novel, key);
-      this.iconPath = Objects.requireNonNullElse(iconPath, key);
-      this.spawnConfiguration = spawnConfiguration;
-      this.variationConfiguration =
-            variationConfiguration.right().orElse(variationConfiguration.left().map(VariationConfiguration::toNewConfiguration).orElse(
-                  new NewVariationConfiguration(false, new ArrayList<>(), new ArrayList<>())));
-   }
+    /**
+     * @param name               the Displayname for the Structure
+     * @param key                the Key of the structure. This identifies this structure
+     * @param spawnConfiguration spawn configuration options of this structure
+     * @param iconPath           the path to the icon of this structure or null to use key's value
+     * @param novel              the path to the structure's Novels or null to use key's value
+     */
+    public StructureData(String name, ResourceLocation key, SpawnConfiguration spawnConfiguration, @Nullable ResourceLocation iconPath, @Nullable ResourceLocation novel,
+                         Either<VariationConfiguration, NewVariationConfiguration> variationConfiguration) {
+        this.name = name;
+        this.key = key;
+        this.novel = Objects.requireNonNullElse(novel, key);
+        this.iconPath = Objects.requireNonNullElse(iconPath, key);
+        this.spawnConfiguration = spawnConfiguration;
+        this.variationConfiguration =
+                variationConfiguration.right().orElse(variationConfiguration.left().map(VariationConfiguration::toNewConfiguration).orElse(
+                        new NewVariationConfiguration(false, new ArrayList<>(), new ArrayList<>())));
+    }
 
-   @Nonnull
-   public static CompoundTag saveToNBT(StructureData structureData) {
-      DataResult<Tag> dataResult = CODEC.encode(structureData, NbtOps.INSTANCE, new CompoundTag());
-      Optional<Tag> optional = dataResult.resultOrPartial(LOGGER::error);
-      return (CompoundTag) optional.filter(value -> value instanceof CompoundTag).orElse(new CompoundTag());
-   }
+    @Nonnull
+    public static CompoundTag saveToNBT(StructureData structureData) {
+        DataResult<Tag> dataResult = CODEC.encode(structureData, NbtOps.INSTANCE, new CompoundTag());
+        Optional<Tag> optional = dataResult.resultOrPartial(LOGGER::error);
+        return (CompoundTag) optional.filter(value -> value instanceof CompoundTag).orElse(new CompoundTag());
+    }
 
-   @Nonnull
-   public static JsonElement saveToJSON(StructureData structureData) {
-      DataResult<JsonElement> dataResult = CODEC.encode(structureData, JsonOps.INSTANCE, new JsonObject());
-      Optional<JsonElement> optional = dataResult.resultOrPartial(LOGGER::error);
-      return optional.orElse(new JsonObject());
-   }
+    @Nonnull
+    public static JsonElement saveToJSON(StructureData structureData) {
+        DataResult<JsonElement> dataResult = CODEC.encode(structureData, JsonOps.INSTANCE, new JsonObject());
+        Optional<JsonElement> optional = dataResult.resultOrPartial(LOGGER::error);
+        return optional.orElse(new JsonObject());
+    }
 
-   @Nullable
-   public static StructureData loadFromNBT(CompoundTag tag) {
-      DataResult<Pair<StructureData, Tag>> dataResult = CODEC.decode(NbtOps.INSTANCE, tag);
-      Optional<Pair<StructureData, Tag>> optional = dataResult.resultOrPartial(LOGGER::error);
-      return optional.map(Pair::getFirst).orElse(null);
-   }
+    @Nullable
+    public static StructureData loadFromNBT(CompoundTag tag) {
+        DataResult<Pair<StructureData, Tag>> dataResult = CODEC.decode(NbtOps.INSTANCE, tag);
+        Optional<Pair<StructureData, Tag>> optional = dataResult.resultOrPartial(LOGGER::error);
+        return optional.map(Pair::getFirst).orElse(null);
+    }
 
-   @Nullable
-   public static StructureData loadFromJSON(JsonElement tag) {
-      DataResult<Pair<StructureData, JsonElement>> dataResult = CODEC.decode(JsonOps.INSTANCE, tag);
-      Optional<Pair<StructureData, JsonElement>> optional = dataResult.resultOrPartial(LOGGER::error);
-      return optional.map(Pair::getFirst).orElse(null);
-   }
+    @Nullable
+    public static StructureData loadFromJSON(JsonElement tag) {
+        DataResult<Pair<StructureData, JsonElement>> dataResult = CODEC.decode(JsonOps.INSTANCE, tag);
+        Optional<Pair<StructureData, JsonElement>> optional = dataResult.resultOrPartial(LOGGER::error);
+        return optional.map(Pair::getFirst).orElse(null);
+    }
 
-   public NewVariationConfiguration getVariationConfiguration() {
-      return variationConfiguration;
-   }
+    public NewVariationConfiguration getVariationConfiguration() {
+        return variationConfiguration;
+    }
 
-   public void setVariationConfiguration(NewVariationConfiguration variationConfiguration) {
-      this.variationConfiguration = variationConfiguration;
-   }
+    public void setVariationConfiguration(NewVariationConfiguration variationConfiguration) {
+        this.variationConfiguration = variationConfiguration;
+    }
 
-   public ResourceLocation getKey() {
-      return key;
-   }
+    public ResourceLocation getKey() {
+        return key;
+    }
 
-   public void setKey(ResourceLocation key) {
-      this.key = key;
-   }
+    public void setKey(ResourceLocation key) {
+        this.key = key;
+    }
 
-   public SpawnConfiguration getSpawnConfiguration() {
-      return spawnConfiguration;
-   }
+    public SpawnConfiguration getSpawnConfiguration() {
+        return spawnConfiguration;
+    }
 
-   public ResourceLocation getNovel() {
-      return novel;
-   }
+    public ResourceLocation getNovel() {
+        return novel;
+    }
 
-   public void setNovel(ResourceLocation novel) {
-      this.novel = novel;
-   }
+    public void setNovel(ResourceLocation novel) {
+        this.novel = novel;
+    }
 
-   public ResourceLocation getIconPath() {
-      return iconPath;
-   }
+    public ResourceLocation getIconPath() {
+        return iconPath;
+    }
 
-   public void setIconPath(ResourceLocation iconPath) {
-      this.iconPath = iconPath;
-   }
+    public void setIconPath(ResourceLocation iconPath) {
+        this.iconPath = iconPath;
+    }
 
-   @Override
-   public int compareTo(StructureData o) {
-      return this.getName().compareTo(o.getName());
-   }
+    @Override
+    public int compareTo(StructureData o) {
+        return this.getName().compareTo(o.getName());
+    }
 
-   public String getName() {
-      return name;
-   }
+    public String getName() {
+        return name;
+    }
 
-   public void setName(String name) {
-      this.name = name;
-   }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/silverminer/shrines/packages/datacontainer/VariationConfiguration.java
+++ src/main/java/com/silverminer/shrines/packages/datacontainer/VariationConfiguration.java
@@ -9,56 +9,47 @@ package com.silverminer.shrines.packages.datacontainer;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.Pair;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class VariationConfiguration extends ForgeRegistryEntry<VariationConfiguration> {
-   public static final Codec<VariationConfiguration> CODEC = RecordCodecBuilder.create(variationConfigurationInstance -> variationConfigurationInstance.group(
-         Codec.BOOL.fieldOf("is_enabled").forGetter(VariationConfiguration::isEnabled),
-         SimpleVariationConfiguration.CODEC.optionalFieldOf("simple", SimpleVariationConfiguration.ALL_DISABLED).forGetter(VariationConfiguration::getSimpleVariationConfiguration),
-         NestedVariationConfiguration.CODEC.optionalFieldOf("nested", NestedVariationConfiguration.ALL_DISABLED).forGetter(VariationConfiguration::getNestedVariationConfiguration)
-   ).apply(variationConfigurationInstance, VariationConfiguration::new));
-   public static final VariationConfiguration ALL_ENABLED = new VariationConfiguration(true, SimpleVariationConfiguration.ALL_ENABLED, NestedVariationConfiguration.ALL_ENABLED);
-   public static final VariationConfiguration ALL_DISABLED = new VariationConfiguration(false, SimpleVariationConfiguration.ALL_DISABLED, NestedVariationConfiguration.ALL_DISABLED);
-   private boolean isEnabled;
-   private SimpleVariationConfiguration simpleVariationConfiguration;
-   private NestedVariationConfiguration nestedVariationConfiguration;
+    public static final Codec<VariationConfiguration> CODEC = RecordCodecBuilder.create(variationConfigurationInstance -> variationConfigurationInstance.group(
+            Codec.BOOL.fieldOf("is_enabled").forGetter(VariationConfiguration::isEnabled),
+            SimpleVariationConfiguration.CODEC.optionalFieldOf("simple", SimpleVariationConfiguration.ALL_DISABLED).forGetter(VariationConfiguration::getSimpleVariationConfiguration),
+            NestedVariationConfiguration.CODEC.optionalFieldOf("nested", NestedVariationConfiguration.ALL_DISABLED).forGetter(VariationConfiguration::getNestedVariationConfiguration)
+    ).apply(variationConfigurationInstance, VariationConfiguration::new));
+    private final SimpleVariationConfiguration simpleVariationConfiguration;
+    private final NestedVariationConfiguration nestedVariationConfiguration;
+    private boolean isEnabled;
 
-   public VariationConfiguration(boolean isEnabled, SimpleVariationConfiguration simpleVariationConfiguration, NestedVariationConfiguration nestedVariationConfiguration) {
-      this.isEnabled = isEnabled;
-      this.simpleVariationConfiguration = simpleVariationConfiguration;
-      this.nestedVariationConfiguration = nestedVariationConfiguration;
-   }
+    public VariationConfiguration(boolean isEnabled, SimpleVariationConfiguration simpleVariationConfiguration, NestedVariationConfiguration nestedVariationConfiguration) {
+        this.isEnabled = isEnabled;
+        this.simpleVariationConfiguration = simpleVariationConfiguration;
+        this.nestedVariationConfiguration = nestedVariationConfiguration;
+    }
 
-   public boolean isEnabled() {
-      return isEnabled;
-   }
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 
-   public void setEnabled(boolean enabled) {
-      isEnabled = enabled;
-   }
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
 
-   public SimpleVariationConfiguration getSimpleVariationConfiguration() {
-      return simpleVariationConfiguration;
-   }
+    public SimpleVariationConfiguration getSimpleVariationConfiguration() {
+        return simpleVariationConfiguration;
+    }
 
-   public void setSimpleVariationConfiguration(SimpleVariationConfiguration simpleVariationConfiguration) {
-      this.simpleVariationConfiguration = simpleVariationConfiguration;
-   }
+    public NestedVariationConfiguration getNestedVariationConfiguration() {
+        return nestedVariationConfiguration;
+    }
 
-   public NestedVariationConfiguration getNestedVariationConfiguration() {
-      return nestedVariationConfiguration;
-   }
-
-   public void setNestedVariationConfiguration(NestedVariationConfiguration nestedVariationConfiguration) {
-      this.nestedVariationConfiguration = nestedVariationConfiguration;
-   }
-
-   public NewVariationConfiguration toNewConfiguration() {
-      List<String> disabledMaterials = this.simpleVariationConfiguration.getDisabledMaterials();
-      List<String> disabledTypes = this.nestedVariationConfiguration.getDisabledTypes();
-      return new NewVariationConfiguration(this.isEnabled, disabledMaterials, disabledTypes);
-   }
+    public NewVariationConfiguration toNewConfiguration() {
+        Pair<List<String>, List<String>> pairDisabledMaterialsTypes = this.simpleVariationConfiguration.getDisabledMaterials();
+        List<String> disabledTypes = this.nestedVariationConfiguration.getDisabledTypes();
+        disabledTypes.addAll(pairDisabledMaterialsTypes.second());
+        return new NewVariationConfiguration(this.isEnabled, pairDisabledMaterialsTypes.first(), disabledTypes);
+    }
 }
