0a95171e944d73806225270df8abe486e039c52c
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-02-27 20:05:16 +0100

    Added licenser gradle plugin & updated license

--- src/main/java/com/silverminer/shrines/ShrinesMod.java
+++ src/main/java/com/silverminer/shrines/ShrinesMod.java
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
 package com.silverminer.shrines;
 
 import com.silverminer.shrines.config.Config;
@@ -23,8 +24,8 @@ public class ShrinesMod {
     public static final String MODID = "shrines";
     public static final Logger LOGGER = LogManager.getLogger(ShrinesMod.class);
     public static final String WIKI_LINK = "https://silverminer007.github.io/ShrinesWiki/";
-    public static String VERSION = "N/A";
     public static final boolean debug = false;
+    public static String VERSION = "N/A";
 
     /**
      * If you're looking for example code on how to make a structure Mod see
@@ -36,9 +37,6 @@ public class ShrinesMod {
                 .ifPresent(container -> VERSION = container.getModInfo().getVersion().toString());
         LOGGER.info("Shrines " + VERSION + " initialized");
         Config.register(ModLoadingContext.get());
-        //NovelsRegistry.NOVELS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        //VariationConfigurationRegistry.VARIATION_CONFIGURATION.register(FMLJavaModLoadingContext.get().getModEventBus());
-        //VariationMaterialsRegistry.VARIATION_MATERIALS.register(FMLJavaModLoadingContext.get().getModEventBus());
         DynamicRegistriesRegistry.NOVELS.register(FMLJavaModLoadingContext.get().getModEventBus());
         DynamicRegistriesRegistry.VARIATION_MATERIAL.register(FMLJavaModLoadingContext.get().getModEventBus());
         DynamicRegistriesRegistry.VARIATION_CONFIGURATION.register(FMLJavaModLoadingContext.get().getModEventBus());
