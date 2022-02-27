3748bdee2ad2d2f99f0e7583d82b0d9f144e751f
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-02-27 15:26:03 +0100

    rearrange build.gradle

--- src/main/java/com/silverminer/shrines/gui/misc/buttons/BooleanValueButton.java
+++ src/main/java/com/silverminer/shrines/gui/misc/buttons/BooleanValueButton.java
@@ -15,47 +15,47 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 public class BooleanValueButton extends Button {
-	public boolean value;
-	private static final ResourceLocation BUTTON_TEXTURE = new ResourceLocation(ShrinesMod.MODID,
-			"textures/gui/buttons_boolean.png");
+    private static final ResourceLocation BUTTON_TEXTURE = new ResourceLocation(ShrinesMod.MODID,
+            "textures/gui/buttons_boolean.png");
+    public boolean value;
 
-	public BooleanValueButton(int posX, int posY, Component text, Button.OnPress onPress, boolean value) {
-		super(posY, posY, 40, 20, text, onPress);
-		this.value = value;
-	}
+    public BooleanValueButton(int posX, int posY, Component text, Button.OnPress onPress, boolean value) {
+        super(posY, posY, 40, 20, text, onPress);
+        this.value = value;
+    }
 
-	public BooleanValueButton(int posX, int posY, Component text, Button.OnPress onPress, Button.OnTooltip tooltip,
-			boolean value) {
-		super(posY, posY, 40, 20, text, onPress, tooltip);
-		this.value = value;
-	}
+    public BooleanValueButton(int posX, int posY, Component text, Button.OnPress onPress, Button.OnTooltip tooltip,
+                              boolean value) {
+        super(posY, posY, 40, 20, text, onPress, tooltip);
+        this.value = value;
+    }
 
-	public void onClick(double mouseX, double mouseY) {
-		this.value = !this.value;
-		super.onClick(mouseX, mouseY);
-	}
+    public void onClick(double mouseX, double mouseY) {
+        this.value = !this.value;
+        super.onClick(mouseX, mouseY);
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void render(PoseStack ms, int mouseX, int mouseY, float partialTicks) {
-		boolean isHovered = this.isMouseOver(mouseX, mouseY);
-		if (this.visible) {
-			Minecraft minecraft = Minecraft.getInstance();
-			RenderSystem.setShaderTexture(0, BUTTON_TEXTURE);
-			RenderSystem.enableDepthTest();
-			RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, this.alpha);
-			RenderSystem.enableBlend();
-			RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA,
-					GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE,
-					GlStateManager.DestFactor.ZERO);
-			RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA,
-					GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-			int buttonH = isHovered ? 20 : 0;
-			buttonH += this.value ? 40 : 0;
-			blit(ms, this.x, this.y, 0, buttonH, this.width, this.height);
-			this.renderBg(ms, minecraft, mouseX, mouseY);
-		}
-	}
+    @Override
+    public void render(@NotNull PoseStack ms, int mouseX, int mouseY, float partialTicks) {
+        boolean isHovered = this.isMouseOver(mouseX, mouseY);
+        if (this.visible) {
+            Minecraft minecraft = Minecraft.getInstance();
+            RenderSystem.setShaderTexture(0, BUTTON_TEXTURE);
+            RenderSystem.enableDepthTest();
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, this.alpha);
+            RenderSystem.enableBlend();
+            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA,
+                    GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE,
+                    GlStateManager.DestFactor.ZERO);
+            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA,
+                    GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            int buttonH = isHovered ? 20 : 0;
+            buttonH += this.value ? 40 : 0;
+            blit(ms, this.x, this.y, 0, buttonH, this.width, this.height);
+            this.renderBg(ms, minecraft, mouseX, mouseY);
+        }
+    }
 }
