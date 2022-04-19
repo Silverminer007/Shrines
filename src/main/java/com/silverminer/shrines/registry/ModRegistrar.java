3b8aaba12b8256ffb8fb27572740fc500fd32822
Silverminer007 <66484505+Silverminer007@users.noreply.github.com>
2022-04-19 15:16:57 +0200

    Update licenses

--- src/main/java/com/silverminer/shrines/registry/ModRegistrar.java
+++ src/main/java/com/silverminer/shrines/registry/ModRegistrar.java
@@ -1,3 +1,11 @@
+/*
+ * Silverminer007
+ * Copyright (c) 2022.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.silverminer.shrines.registry;
 
 import net.minecraft.core.Registry;
